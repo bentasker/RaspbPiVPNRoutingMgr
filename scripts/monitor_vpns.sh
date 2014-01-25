8d3f4efb6dd16639021285ca2047eefed1c55e86
Ben Tasker <github@bentasker.co.uk>
2014-01-25 05:36:43 +0000

    Created basic monitoring script

---
+++ scripts/monitor_vpns.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+# VPN Tunnel maintenance script
+#
+# Copyright (C) 2014 B Tasker
+# Released under GNU GPL V2
+# See http://www.gnu.org/licenses/gpl-2.0.html
+#
+# Make sure your openvpn files contain 'daemon'
+#
+# Create the following file, with the following contents (change to suit)
+#
+# /root/.vpn_config
+# INSTDIR='/root/VPNs'
+#
+
+
+# Load the config
+source /root/.vpn_config
+
+
+# Default our change monitor to No
+CHANGES='no'
+
+# Cycle through each of the VPNs
+for i in `cat $INSTDIR/config/VPNS`
+do
+
+  # See if there's an instance running for this vpn
+
+  ps aux | grep openvpn | grep "$i.conf" > /dev/null
+  if [ "$?" == "1" ]
+  then
+    # Link seems to be down, bring it back up
+
+    cd $INSTDIR/config/VPNs/$i
+    openvpn "${i}.conf"
+
+    # We've made changes
+    CHANGES='yes'
+
+  fi
+
+done
+
+
+# If we've brought a tunnel back up, we're probably going to want to resurrect routes etc
+if [ "$CHANGES" == "yes" ]
+then
+
+  # Allow time for the tunnels to come up
+  sleep 10
+
+  source $INSTDIR/config/routes
+  source $INSTDIR/config/firewall
+
+fi
